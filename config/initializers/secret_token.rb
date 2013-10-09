5cf5d5715cda9cac2c422b26ba62904dc066d582
Joy <jglo75@yahoo.com>
2013-10-09 18:19:36 -0400

    Updated Gemfile for deployment to Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '6941a24635b322979b43945673fa27c4ca5819b1550a92eadc29aedd1f58602664f507ed4d1eda3545357933f3d33266f302587035eb92e798bae11b1008c218'
